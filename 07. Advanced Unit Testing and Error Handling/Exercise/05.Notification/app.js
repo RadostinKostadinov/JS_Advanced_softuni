c8a87c36fb5160c2d7687006e4dac004be432769
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2021-02-07 16:45:20 +0200

    Advanced Unit Testing and Error Handling Exercises

--- 07. Advanced Unit Testing and Error Handling/Exercise/05.Notification/app.js
+++ 07. Advanced Unit Testing and Error Handling/Exercise/05.Notification/app.js
@@ -1,4 +1,11 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  const notificationDiv = document.getElementById('notification');
+  notificationDiv.style.display = 'block';
+  notificationDiv.textContent = message;
+  notificationDiv.addEventListener('click', onClickHideDiv);
+
+  function onClickHideDiv(ev){
+    ev.target.style.display = 'none';
+  }
+
 }
