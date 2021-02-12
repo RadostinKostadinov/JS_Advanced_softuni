b14b566e4a92849f3bfdd9b17f4ed09f114c9e5f
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2021-02-12 21:58:44 +0200

    Prototypes and Inheritance Ex

--- 09. Prototypes and Inheritance/Exercise/baloons.js
+++ 09. Prototypes and Inheritance/Exercise/baloons.js
@@ -10,23 +10,30 @@ function solve() {
     class PartyBalloon extends Balloon {
         constructor(color, gasWeight, ribbonColor, ribbonLength){
             super(color, gasWeight);
-            this._ribbon = {
-                ribbonColor,
-                ribbonLength,
+            this.ribbon = {
+                color: ribbonColor,
+                length: ribbonLength,
             };
         }
         get ribbon() {
             return this._ribbon;
+        }
+
+        set ribbon(ribbon) {
+            this._ribbon = ribbon;
         }
     }
 
     class BirthdayBalloon extends PartyBalloon {
         constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
             super(color, gasWeight, ribbonColor, ribbonLength);
-            this._text = text;
+            this.text = text;
         }
         get text() {
             return this._text;
+        }
+        set text(text) {
+            this._text = text;
         }
     }
 
